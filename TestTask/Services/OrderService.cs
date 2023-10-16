d3bf95ecfccda61c2cd01974c2910ab0da20ea40
Azure <Azure@DESKTOP-LUN14ML>
2023-10-16 18:05:52 +0300

    Add project files.

---
+++ TestTask/Services/OrderService.cs
@@ -0,0 +1,20 @@
+﻿using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services
+{
+    public class OrderService : IOrderService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public OrderService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task<Order> GetOrder() => Task.FromResult(_context.Orders.OrderBy(o => o.Price * o.Quantity).Last());
+
+        public Task<List<Order>> GetOrders() => Task.FromResult(_context.Orders.Where(o => o.Quantity > 10).ToList());
+    }
+}
---
+++ TestTask/Services/UserService.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Enums;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task<User> GetUser() => Task.FromResult(_context.Users.OrderBy(u => u.Orders.Count).Last());
+
+        public Task<List<User>> GetUsers() => Task.FromResult(_context.Users.Where(u => u.Status == UserStatus.Inactive).ToList());
+    }
+}
